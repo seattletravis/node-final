a40166e1478e78b414c3de32eb5659a1b54e0b8b
Travis Lamberte <travis@lamberte.com>
2024-01-25 09:55:23 -0800

    add more

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,26 +11,41 @@ public_users.post('/register', (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-	//Write your code here
 	res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-	//Write your code here
-	return res.status(300).json({ message: 'Yet to be implemented' });
+	const isbn = req.params.isbn;
+	res.send(books[isbn]);
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-	//Write your code here
-	return res.status(300).json({ message: 'Yet to be implemented' });
+	const author = req.params.author;
+	const keys = [];
+	for (const [key, _] of Object.entries(books)) {
+		keys.push(key);
+	}
+	keys.forEach((el) => {
+		if (author === books[el].author) {
+			res.send(books[el]);
+		}
+	});
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-	//Write your code here
-	return res.status(300).json({ message: 'Yet to be implemented' });
+	const title = req.params.title;
+	const keys = [];
+	for (const [key, _] of Object.entries(books)) {
+		keys.push(key);
+	}
+	keys.forEach((el) => {
+		if (title === books[el].title) {
+			res.send(books[[el]]);
+		}
+	});
 });
 
 //  Get book review
