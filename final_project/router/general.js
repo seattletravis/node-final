a0d2030d05ac8f4a4108d45b24d5b6dc185fe170
Travis Lamberte <travis@lamberte.com>
2024-01-25 16:56:48 -0800

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,23 +20,25 @@ public_users.post('/register', (req, res) => {
 	return res.status(404).json({ message: 'Unable to register user.' });
 });
 
-// Get the book list available in the shop - ASYNC
-// public_users.get('/', async function (req, res) {
-// 	const response = await fetch(books);
-// 	res.send(JSON.stringify(books, null, 4));
-// 	res.send(response);
-// });
-
 public_users.get('/', function (req, res) {
-	res.send(JSON.stringify(books, null, 4));
+	try {
+		const bookList = books;
+		res.json(bookList); // Neatly format JSON output
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Error retrieving book list' });
+	}
 });
 
-// Get book details based on ISBN - ASNYC
-public_users.get('/isbn/:isbn', async function (req, res) {
-	// const isbn = req.params.isbn;
-	const response = await fetch(books[isbn]);
-	// res.send(books[isbn]);
-	res.send(response);
+// Get the book list available in the shop
+public_users.get('/', async function (req, res) {
+	try {
+		const booksAsync = await getBooks();
+		res.send(JSON.stringify(booksAsync, null, 4));
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Internal Server Error');
+	}
 });
 
 // Get book details based on ISBN
